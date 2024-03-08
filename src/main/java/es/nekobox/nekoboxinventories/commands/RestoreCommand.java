ce53374af416a5cca13ef10d7a5ff038f567fdfa
FroostySnoowman <jacob.j.beal@icloud.com>
2024-03-07 18:56:53 -0700

    Removed Unused Import

--- src/main/java/es/nekobox/nekoboxinventories/commands/RestoreCommand.java
+++ src/main/java/es/nekobox/nekoboxinventories/commands/RestoreCommand.java
@@ -16,7 +16,6 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.List;
 
 public class RestoreCommand implements CommandExecutor {
